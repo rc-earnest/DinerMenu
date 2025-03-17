c4d536627991160e5741c1588af442861de77213
rc-earnest <rc.earnest04@gmail.com>
2025-03-16 20:05:49 -0600

    added comments for the code, I didn't realize I hadn't done the source control until I was finished writing the last "initial" section.

--- DinerMenu/DinerMenuForm.vb
+++ DinerMenu/DinerMenuForm.vb
@@ -1,4 +1,9 @@
 ﻿Public Class DinerMenuForm
+    ''' <summary>
+    ''' On startup this sets the tool tips and the defaults for each item.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
     Private Sub DinerMenuForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         setDefaults()
         DisplaySpecialLabel.TextAlign = ContentAlignment.MiddleCenter
@@ -8,25 +13,40 @@
         ToolTip1.SetToolTip(ExitButton, "Exits the program")
 
     End Sub
+    ''' <summary>
+    ''' This is the code that will set the defaults in the system.
+    ''' </summary>
     Sub setDefaults()
         DisplaySpecialLabel.TextAlign = ContentAlignment.MiddleCenter
         DisplaySpecialLabel.Text = ""
     End Sub
-
+    ''' <summary>
+    ''' Displays the Salad selection in Label 2 which is the "DisplaySpectialLabel".
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
     Private Sub SaladButton_Click(sender As Object, e As EventArgs) Handles SaladButton.Click
         DisplaySpecialLabel.TextAlign = ContentAlignment.MiddleCenter
         DisplaySpecialLabel.Text = $"~ The Finest Fried Chicken BBQ Ceasar Salad ~
 
 Made exclusively from the chickens raised in mister fredricks back yard and the ceasar dressing, well that is a family secret."
     End Sub
-
+    ''' <summary>
+    ''' Displays the Soup selection in Label 2 which is the "DisplaySpectialLabel".
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
     Private Sub SoupButton_Click(sender As Object, e As EventArgs) Handles SoupButton.Click
         DisplaySpecialLabel.TextAlign = ContentAlignment.MiddleCenter
         DisplaySpecialLabel.Text = $"~ Fried Split-Pea Soup ~
 
 Don't ask us because we wont tell you how or what it is."
     End Sub
-
+    ''' <summary>
+    ''' Displays the Fish selection in Label 2 which is the "DisplaySpectialLabel".
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
     Private Sub FishButton_Click(sender As Object, e As EventArgs) Handles FishButton.Click
         DisplaySpecialLabel.TextAlign = ContentAlignment.MiddleCenter
         DisplaySpecialLabel.Text = $"~ Fish and Chips ~
@@ -34,7 +54,11 @@ Don't ask us because we wont tell you how or what it is."
 
 Hopefully you like vinegar because it is chock full of the stuff."
     End Sub
-
+    ''' <summary>
+    ''' Closes the program once the exit button is activated or clicked.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
     Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
         Me.Close()
     End Sub
